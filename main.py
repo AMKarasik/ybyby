8d0921b2265ec10bbb713a61630f00d92696715d
Karasik Andrei <andrei.karasik@math.msu.ru>
2024-03-19 21:55:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import requests
 import json
+import os
+
+output_dir = "region_data"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 app = FastAPI()
 
@@ -17,9 +22,33 @@ app.add_middleware(
 )
 
 df = pd.read_csv('cum.csv', sep=',')
+df['ФИАС Регион'] = df['ФИАС Регион'].str.replace('г ', '')
+df['ФИАС Регион'] = df['ФИАС Регион'].str.replace('обл', 'область')
+df['ФИАС Регион'] = df['ФИАС Регион'].str.replace('Респ', 'Республика')
+df['ФИАС Регион'] = df['ФИАС Регион'].str.replace('АО ', '')
+
+df['ФИАС Регион'] = df['ФИАС Регион'].str.replace('Аобласть Еврейская', 'Еврейская область')
+df['ФИАС Регион'] = df['ФИАС Регион'].str.replace('Кемеровская областьасть - Кузбасс область', 'Кемеровская область')
+df['ФИАС Регион'] = df['ФИАС Регион'].str.replace('Чувашия Чувашская Республикаублика -', 'Чувашская область')
+df['ФИАС Регион'] = df['ФИАС Регион'].str.replace('Республика Донецкая Народная', 'Донецкая область')
+df['ФИАС Регион'] = df['ФИАС Регион'].str.replace('Республика Луганская Народная', 'Луганская область')
+df['ФИАС Регион'] = df['ФИАС Регион'].str.replace('Республика Саха /Якутия/', 'Республика Саха')
+df['ФИАС Регион'] = df['ФИАС Регион'].str.replace('Ханты-Мансийский Автономный окру- Югра', 'Ханты-Мансийский округ')
+
+df = df[:1000]
+
+unique_regions = df['ФИАС Регион'].unique()
+
+region_dfs = {}
+for region in unique_regions:
+    region_dfs[region] = df[df['ФИАС Регион'] == region]
+
+for region, region_df in region_dfs.items():
+    output_file = os.path.join(output_dir, f"{region}.csv")
+    region_df.to_csv(output_file, index=False)
 
 result = df.to_dict(orient='records')
-regions = sorted(list(set(df["ФИАС Регион"])))
+regions = [os.path.splitext(f)[0] for f in os.listdir('region_data') if f.endswith('.csv')][:10]
 
 #%%
 with open('map.html', 'r', encoding='utf-8') as f:
